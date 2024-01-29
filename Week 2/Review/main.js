11d1eb2ed4380ef7570d8465f4a73d556d0c4707
Grete Tanberg <97848166+allingoodfaith@users.noreply.github.com>
2024-01-29 15:11:07 -0700

    week 2 homework

---
+++ Week 2/Review/main.js
@@ -0,0 +1,20 @@
+/*
+Author: Grete Tanberg
+File Name: Homework Week 2 Page.html
+
+Date: 01/23/2024
+Purpose: Week 2 Homework for web tech
+*/
+
+console.log("Week 2 Homework");
+console.log('Favorite Games');
+console.log('Age of Mythology'+ ' '+'Minecraft'+ ' ' +'It Takes 2');
+console.log('Favorite Website');
+console.log('Crunchy Roll'+ ' '+'Youtube'+ ' ' +'Tumblr');
+console.log('Favorite Artists');
+console.log('Eiichiro Oda');
+console.log('Created One piece. nuff said');
+console.log('Ryoko Kui');
+console.log('Creator of Dungeon Meshi, one of the best fantasy manga I have ever read. Amazing naturalistic art and character design.');
+console.log('Kody Fry');
+console.log('One of my favorite musical composers. Really turns back to that early Disney style that just is wonderful to listen too.');
