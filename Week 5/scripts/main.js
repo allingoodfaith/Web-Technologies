8d81a0ba2dbda3a825fbaf812fb2a02af45c5272
Grete Tanberg <97848166+allingoodfaith@users.noreply.github.com>
2024-02-19 22:09:21 -0700

    weeks 4 and 5

---
+++ Week 5/scripts/main.js
@@ -0,0 +1,35 @@
+
+var imageTags = ["image1", "image2", "image3", "image4"];
+var blankImagePath = "images/card back.png";
+var actualImages = new Array();
+    
+function printBlanks()
+{  
+    createRandomImageArray();
+    for(var i = 0; i < imageTags.length; i++)
+    {
+        document.getElementById(imageTags[i]).src= blankImagePath;
+    }
+       
+}
+
+function createRandomImageArray()
+{
+    var actualImagePath = ["images/card_1.png","images/card_2.png" ];
+    var count = [0,0];
+    while(actualImages.length < 4)
+    {
+        var randomNumber = Math.floor(Math.random() * actualImagePath.length)
+        if(count[randomNumber] < 2)
+        {
+            actualImages.push(actualImagePath[randomNumber]);
+            count[randomNumber] = count[randomNumber] + 1;
+        }
+    }   
+}
+
+function flipImage(number)
+{
+    document.getElementById(imageTags[number]).src= actualImages[number];      
+    
+}
